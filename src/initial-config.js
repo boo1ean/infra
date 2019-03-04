04806bf6fa2824bd37f21b8e3ad8107c8e779514
boo1ean <boo1ean0807@gmail.com>
2019-03-04 19:30:08 +0200

    Update initial config

--- src/initial-config.js
+++ src/initial-config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    "activeProject": {
-		"name": "boxes",
-		"path": "/home/kmenux/src/boxes",
-		"servicesConfigPath": "/home/kmenux/src/boxes/boxes-infra/config.js",
-    },
-	"projects": [{
-		"name": "boxes",
-		"path": "/home/kmenux/src/boxes",
-		"servicesConfigPath": "/home/kmenux/src/boxes/boxes-infra/config.js",
-	}]
+    "activeProject": {},
+	"projects": [],
 }
