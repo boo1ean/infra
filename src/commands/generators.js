9e7ef7b3f3e4cee290fd1e6b4c7a45e5578b8477
boo1ean <boo1ean0807@gmail.com>
2019-06-07 16:00:22 +0300

    Implement basic postgres service generation script

--- src/commands/generators.js
+++ src/commands/generators.js
@@ -10,8 +10,23 @@ const utils = require('../utils')
 const scaffolder = require('../scaffolder')
 const conf = new Conf()
 
+const COMPOSE_CONFIG_VERSION = 3.7
+
 const dockerComposeConfigPath = path.resolve(conf.get('activeProject.path'), 'docker-compose.yml')
+
+if (!fs.existsSync(dockerComposeConfigPath)) {
+	fs.writeFileSync(dockerComposeConfigPath, `version: "${COMPOSE_CONFIG_VERSION}"`)
+}
+
 const dockerComposeConfig = yaml.parse(fs.readFileSync(dockerComposeConfigPath).toString())
+
+const config = {
+	pg: {
+		default_user: 'root',
+		default_password: 'root',
+		default_db: 'samples',
+	}
+}
 
 module.exports = yargs => {
 	return yargs
@@ -35,14 +50,59 @@ module.exports = yargs => {
 			['api <name>'],
 			'Create express app in active project',
 			_.noop,
-			createApiApp('express-api')
+			createServiceFromTemplate('express-api')
 		)
 
 		.command(
 			['api-workframe <name>', 'api-wf <name>'],
 			'Create workframe-powered express app in active project',
 			_.noop,
-			createApiApp('express-api-workframe')
+			createServiceFromTemplate('express-api-workframe')
+		)
+
+		.command(
+			['postgres [name]', 'pg [name]'],
+			'Create postgres service in active project',
+			_.noop,
+			argv => {
+				const name = argv.name || 'postgres'
+				const serviceName = `${conf.get('activeProject.name')}-${name}`
+				const destPath = path.resolve(
+					conf.get('activeProject.path'),
+					serviceName
+				)
+
+				console.log('Generating service: %s', chalk.bold(serviceName))
+				console.log('Under path: %s', chalk.bold(destPath))
+				scaffolder.generate('postgres', destPath)
+
+				const volumeName = `${serviceName}-data`
+				if (!dockerComposeConfig.services) {
+					dockerComposeConfig.services = {}
+				}
+				dockerComposeConfig.services[serviceName] = {
+					build: `./${serviceName}`,
+					restart: 'always',
+					environment: {
+						POSTGRES_USER: config.pg.default_user,
+						POSTGRES_PASSWORD: config.pg.default_password,
+						POSTGRES_DB: config.pg.default_db,
+					},
+					volumes: [
+						`${volumeName}:/var/lib/postgresql/data`,
+					],
+				}
+
+				if (!dockerComposeConfig.volumes) {
+					dockerComposeConfig.volumes = {}
+				}
+				dockerComposeConfig.volumes[volumeName] = { driver: 'local' }
+
+				writeDockerComposeConfig(dockerComposeConfig)
+
+				console.log('Updated docker-compose.yml')
+				console.log(chalk.green('Success!'))
+			}
 		)
 
 		.command(['workframe-api <name>', 'wf-api <name>'], 'Create workframe-powered express app in active project', _.noop, argv => {
@@ -58,7 +118,7 @@ module.exports = yargs => {
 		.help()
 }
 
-function createApiApp (templateName) {
+function createServiceFromTemplate (templateName) {
 	return argv => {
 		const serviceName = `${conf.get('activeProject.name')}-${argv.name}`
 		const destPath = path.resolve(
@@ -75,6 +135,7 @@ function createApiApp (templateName) {
 		}
 		dockerComposeConfig.services[serviceName] = {
 			build: `./${serviceName}`,
+			restart: 'always',
 		}
 		writeDockerComposeConfig(dockerComposeConfig)
 		console.log('Updated docker-compose.yml')
