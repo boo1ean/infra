2f874eb8894064b1eb87f933fdc80cfe4fc80ef2
boo1ean <boo1ean0807@gmail.com>
2019-06-11 19:01:28 +0300

    Update utils to use docker-compose configs

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,33 @@
+const path = require('path')
 const Conf = require('conf')
+const yaml = require('yaml')
+const fs = require('fs')
+
 const conf = new Conf()
 
-function getServicesConfig () {
-	return require(conf.get('activeProject.servicesConfigPath')).services
+function getServicesNames () {
+	return Object.keys(getDockerComposeConfig().services)
 }
 
-function getServiceNames () {
-	return Object.keys(getServicesConfig())
+function getDockerComposeConfig () {
+	return readYaml(path.resolve(
+		conf.get('activeProject.path'),
+		'docker-compose.yml'
+	))
+}
+function getDevDockerComposeConfig () {
+	return readYaml(path.resolve(
+		conf.get('activeProject.path'),
+		'dev.docker-compose.yml'
+	))
+}
+
+function readYaml (path) {
+	return yaml.parse(fs.readFileSync(path).toString())
 }
 
 module.exports = {
-	getServicesConfig,
-	getServiceNames,
+	getDockerComposeConfig,
+	getDevDockerComposeConfig,
+	getServicesNames,
 }
