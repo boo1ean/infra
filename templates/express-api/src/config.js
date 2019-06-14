0bc578192b749cae411fac13b32ab4a68021ca0f
boo1ean <boo1ean0807@gmail.com>
2019-06-14 17:25:42 +0300

    Improve templates

--- templates/express-api/src/config.js
+++ templates/express-api/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	name: 'express-app',
-	port: 3000,
+	port: process.env.PORT || 3000,
 }
--- templates/express-api/src/index.js
+++ templates/express-api/src/index.js
@@ -3,7 +3,7 @@ const config = require('./config')
 const app = express()
 
 app.all('*', (req, res) => {
-	res.send('Hello, world!')
+	res.json([1, 2, 3])
 })
 
 app.listen(config.port, () => {
